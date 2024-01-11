4ca324b9af403bc6caa1905f3e2e7008ba5cafe5
突突兔 <37355028+tututu-patch@users.noreply.github.com>
2024-01-11 14:01:14 +0800

    Update LStringType.java

--- src/unluac/parse/LStringType.java
+++ src/unluac/parse/LStringType.java
@@ -101,18 +101,19 @@ class LStringType53 extends LStringType {
       }
       
     });
-    int x=size-1;
+
     if (b.length() > 0) {
+      int x=size-1;
       int v5=b.charAt(0)^x;
+      int v6=x+v5;
+      char v8=(char)x;
+      for(int i=0;i<size-1;i++){
+        v8=(char)((char)x%255);
+        x+=v6;
+        b.setCharAt(i,(char)(((char)b.charAt(i))^((char)v8)));
+      }
     }
-    // int v5=b.charAt(0)^x;
-    int v6=x+v5;
-    char v8=(char)x;
-    for(int i=0;i<size-1;i++){
-	    v8=(char)((char)x%255);
-	    x+=v6;
-	    b.setCharAt(i,(char)(((char)b.charAt(i))^((char)v8)));
-    }
+
     String s = b.toString();
     if(header.debug) {
       System.out.println("-- parsed <string> \"" + s + "\"");
